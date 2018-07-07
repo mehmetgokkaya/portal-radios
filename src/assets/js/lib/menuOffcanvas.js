61a4a5d9969d2d58c5948c8e34a59cd3dccfb61f
João Teodoro <joaotdn@gmail.com>
2018-07-07 18:40:33 -0300

    apresentação

---
+++ src/assets/js/lib/menuOffcanvas.js
@@ -0,0 +1,6 @@
+export default function () {
+    const elementToClick = document.querySelectorAll('[data-toggle-class]');
+    if (elementToClick.length >= 1) {
+        console.log('teu cu')
+    }
+}
