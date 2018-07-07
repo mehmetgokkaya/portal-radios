94db3f56ccd1b4648dea0aca0dac0f8f56c92b60
João Teodoro <joaotdn@gmail.com>
2018-07-07 20:12:50 -0300

    desenvolvendo single

--- src/assets/js/lib/menuOffcanvas.js
+++ src/assets/js/lib/menuOffcanvas.js
@@ -1,6 +1,16 @@
 export default function () {
-    const elementToClick = document.querySelectorAll('[data-toggle-class]');
-    if (elementToClick.length >= 1) {
-        console.log('teu cu')
+    const toggleOffcanvas = document.querySelectorAll('.toggle-offcanvas');
+
+    if (toggleOffcanvas.length >= 1) {
+        toggleOffcanvas.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+                const menuOffcanvas = document.querySelector('.menu-offcanvas');
+                const closeOffcanvas = document.querySelector('.close-menu-offcanvas');
+
+                menuOffcanvas.classList.toggle('show');
+                closeOffcanvas.classList.toggle('show');
+            }, false);
+        })
     }
 }
